335eced5ff008e2f13a4facbab1ded2b67765f30
Diego Agüero <diegoaguero555@gmail.com>
2026-01-14 17:30:16 -0300

    Fix: Ajustes en Phantom Camera y corrección de Trigger Zones
    - Solucionado bug de retorno de cámara en zonas de enfoque (TriggerCam).
    - Ajustes en la configuración de la PlayerCam (Modo Framed y Tweens).
    - Estabilización del sistema de cámara tras la muerte del jugador.
    - Nota: Pendiente ajuste fino de márgenes verticales en algunos niveles.

--- Scripts/trigger_cam.gd
+++ Scripts/trigger_cam.gd
@@ -3,11 +3,11 @@ extends Area2D
 @export var zone_camera: PhantomCamera2D 
 
 func _on_body_entered(body):
-    if body.is_in_group("Player"):
-        if zone_camera.get_follow_target() == null:
-            zone_camera.set_follow_target(body)
-        zone_camera.set_priority(20)
+	if body.is_in_group("Player"):
+		if zone_camera.get_follow_target() == null:
+			zone_camera.set_follow_target(body)
+		zone_camera.set_priority(20)
 
 func _on_body_exited(body):
-    if body.is_in_group("Player"):
-        zone_camera.set_priority(0)
+	if body.is_in_group("Player"):
+		zone_camera.set_priority(0)
